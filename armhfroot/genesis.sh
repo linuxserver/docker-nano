c07f310ef85a9e72ab9017af42c208bc6d88e591
thelamer <ryankuba@gmail.com>
2020-05-18 18:19:47 -0700

    expanding docs and fixing bug in armhf image

--- armhfroot/genesis.sh
+++ armhfroot/genesis.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-echo `#######################################################################
+echo '#######################################################################
 THIS IMAGE IS ONLY AVAILABLE FOR 64 BIT SYSTEMS
 IF YOU ARE USING RASPIAN ON A MODERN PI (3 or 4) CONSIDER USING UBUNTU
 #######################################################################'
